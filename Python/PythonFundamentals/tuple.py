1258c51e5906ea620a632546275ec78de5d5604c
shelendrajain2004 <105582192+shelendrajain2004@users.noreply.github.com>
2022-05-15 15:26:11 +0530

    Create tuple.py

---
+++ Python/PythonFundamentals/tuple.py
@@ -0,0 +1,10 @@
+
+def tuple_test():
+    car = ('tesla', 'ford', 'skoda')
+    print(car)
+    print(len(car))
+    print ('tesla' in car)
+
+if __name__ == '__main__':
+    print (f'Hello')
+    tuple_test()
